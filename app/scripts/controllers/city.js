7b2c63a84131558ac3fcb5ad4141913c437c92d5
yaskyj <yaskyj@yahoo.com>
2015-04-07 21:39:11 -0500

    Add new controller and view

---
+++ app/scripts/controllers/city.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zyringApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zyringApp
+ */
+angular.module('zyringApp')
+  .controller('CityCtrl', function ($scope) {
+});
