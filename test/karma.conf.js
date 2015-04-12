c13a19b4cc60c1987cfcecf90e5753cb7c66c49e
yaskyj <yaskyj@yahoo.com>
2015-04-12 00:58:29 -0500

    Added angular bootstrap

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -38,6 +38,7 @@ module.exports = function(config) {
       'bower_components/masonry/masonry.js',
       'bower_components/imagesloaded/imagesloaded.js',
       'bower_components/angular-masonry/angular-masonry.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
