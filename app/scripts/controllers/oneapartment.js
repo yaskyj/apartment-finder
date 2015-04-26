134b6737cce34eae4d699d688ef3d85bfea01d7d
yaskyj <yaskyj@yahoo.com>
2015-04-25 19:26:29 -0500

    Individual apartment links work

--- app/scripts/controllers/oneapartment.js
+++ app/scripts/controllers/oneapartment.js
@@ -1,6 +1,6 @@
 'use strict';
 
 apartmentFinder
-  .controller('OneApartmentCtrl', ['$scope', '$routeparams', function($scope, $routeparams) {
-    $scope.apartmentId = $routeparams.id
+  .controller('OneApartmentCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+    $scope.apartmentId = $routeParams.id
   }]);
