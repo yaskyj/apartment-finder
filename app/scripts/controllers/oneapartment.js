db7dbb3f6c57564a72bf467d0bc797faf76f17ec
yaskyj <yaskyj@yahoo.com>
2015-04-26 11:03:45 -0500

    Complete single apartment page

--- app/scripts/controllers/oneapartment.js
+++ app/scripts/controllers/oneapartment.js
@@ -4,6 +4,6 @@ apartmentFinder
   .controller('OneApartmentCtrl', ['$scope', '$routeParams', 'oneApartment', function($scope, $routeParams, oneApartment) {
     $scope.apartmentId = $routeParams.id
     oneApartment.get({id: $scope.apartmentId}, function(data) {
-      console.log(data);
+      $scope.singleApartment = data;
     });
   }]);
